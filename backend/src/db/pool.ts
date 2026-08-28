e35ae3de41bbfa429a59fdda69a2b81f5c514f53
fdevinar <fc_devinar@hotmail.com>
2026-08-28 12:38:36 -0300

    created player cache on db via schema file

--- backend/src/db/pool.ts
+++ backend/src/db/pool.ts
@@ -1,5 +1,6 @@
 import { Pool } from "pg";
 import { config } from '../config.js'
+import { readFile } from "node:fs/promises";
 
 const pool = new Pool( {connectionString: config.databaseUrl} );
 
@@ -8,5 +9,21 @@ const pool = new Pool( {connectionString: config.databaseUrl} );
 // if (result.rowCount) {
 //   console.log("Pool Query OK");
 // }
+
+const sqlUrl = new URL('./schema.sql', import.meta.url);
+const sqlQuery = await readFile(sqlUrl, 'utf8');
+
+try {
+    const res = await pool.query(sqlQuery);
+    console.log("✅ Table created successfully!");
+} catch(err) {
+    if (err instanceof Error) {
+        console.error("❌ Error creating table:", err.stack);
+    } else {
+        console.error("❌ Unknown error", err);    
+    }
+} finally {
+    pool.end();
+}
 
 export default pool;
