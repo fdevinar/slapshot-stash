45d27838b12fd6417102b26bd8d14b640b441eb2
fdevinar <fc_devinar@hotmail.com>
2026-09-03 20:05:34 -0300

    get all sets route and service

--- backend/src/routes/sets.ts
+++ backend/src/routes/sets.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import type { Request,Response } from "express";
-import createSet from "../services/sets.js";
+import {createSet, getSets} from "../services/sets.js";
 
 const router = Router();
 
@@ -13,6 +13,12 @@ router.post('/', async (req: Request, res: Response) => {
     const newSet = await createSet(name);
     console.log(newSet);
     res.status(201).json(newSet);    
+});
+
+router.get('/', async (req: Request, res: Response) => {    
+    const allSets = await getSets();
+    console.log(allSets);
+    res.status(200).json(allSets);
 });
 
 export default router;
--- backend/src/services/sets.ts
+++ backend/src/services/sets.ts
@@ -5,10 +5,16 @@ interface Set {
     name: string;
 }
 
-async function createSet(name: string): Promise<Set> {
+export async function createSet(name: string): Promise<Set> {
     const sqlQuery = 'INSERT INTO sets (name) VALUES ($1) RETURNING *;';    
     const result = await pool.query(sqlQuery, [name]);
     return result.rows[0];
 }
 
-export default createSet;
+export async function getSets(): Promise<Array<Set>> {
+    const sqlQuery = 'SELECT * FROM sets;';    
+    const result = await pool.query(sqlQuery);
+    return result.rows;
+}
+
+// export default createSet;
