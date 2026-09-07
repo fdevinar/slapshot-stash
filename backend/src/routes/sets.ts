3eb58de451fcff340ad632735321f8690a358636
fdevinar <fc_devinar@hotmail.com>
2026-09-07 15:37:35 -0300

    created update and delete sets routes and service

--- backend/src/routes/sets.ts
+++ backend/src/routes/sets.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import type { Request,Response } from "express";
-import {createSet, getSets, getSetbyId} from "../services/sets.js";
+import {createSet, getSets, getSetbyId, updateSetById, deleteSetbyId} from "../services/sets.js";
 
 const router = Router();
 
@@ -18,13 +18,11 @@ router.post('/', async (req: Request, res: Response) => {
     console.log(newSet);
     res.status(201).json(newSet);    
 });
-
 router.get('/', async (req: Request, res: Response) => {    
     const allSets = await getSets();
     console.log(allSets);
     res.status(200).json(allSets);
 });
-
 router.get('/:id', async (req: Request<UserRouteParams>, res: Response) => {    
     const setId = parseInt(req.params.id, 10);
     if (isNaN(setId)) {
@@ -34,6 +32,40 @@ router.get('/:id', async (req: Request<UserRouteParams>, res: Response) => {
     const set = await getSetbyId(setId);
     if (!set) {
         res.status(404).json({error: 'Set not found'});
+        return;
+    }
+    console.log(set);
+    res.status(200).json(set);
+});
+router.put('/:id', async (req: Request<UserRouteParams>, res: Response) => {
+    const setId = parseInt(req.params.id, 10);
+    if (isNaN(setId)) {
+        res.status(400).json({error: 'ID must be a valid number'});
+        return;
+    }
+    const name = req.body.name;
+    if (typeof name !== 'string' || name.trim() === '') {
+        res.status(400).json({error: 'Name is required'});
+        return;
+    }
+    const set = await updateSetById(setId, name);
+    if (!set) {
+        res.status(404).json({error: 'Set not found'});
+        return;
+    }
+    console.log(set);
+    res.status(200).json(set);
+});
+router.delete('/:id', async (req: Request<UserRouteParams>, res: Response) => {    
+    const setId = parseInt(req.params.id, 10);
+    if (isNaN(setId)) {
+        res.status(400).json({error: 'ID must be a valid number'});
+        return;
+    }
+    const set = await deleteSetbyId(setId);
+    if (!set) {
+        res.status(404).json({error: 'Set not found'});
+        return;
     }
     console.log(set);
     res.status(200).json(set);
--- backend/src/services/sets.ts
+++ backend/src/services/sets.ts
@@ -10,15 +10,24 @@ export async function createSet(name: string): Promise<Set> {
     const result = await pool.query(sqlQuery, [name]);
     return result.rows[0];
 }
-
 export async function getSets(): Promise<Array<Set>> {
     const sqlQuery = 'SELECT * FROM sets;';    
     const result = await pool.query(sqlQuery);
     return result.rows;
 }
-
 export async function getSetbyId(id: number): Promise<Set | undefined> {
     const sqlQuery = 'SELECT * FROM sets WHERE id = $1;';
+    const result = await pool.query(sqlQuery, [id]);
+    return result.rows[0];
+}
+export async function updateSetById(id: number, value: string): Promise<Set | undefined> {
+    const values = [id, value];
+    const sqlQuery = 'UPDATE sets SET name = $2 WHERE id = $1 RETURNING *;';
+    const result = await pool.query(sqlQuery, values);
+    return result.rows[0];
+}
+export async function deleteSetbyId(id: number): Promise<Set | undefined> {
+    const sqlQuery = 'DELETE FROM sets WHERE id = $1 RETURNING *;';
     const result = await pool.query(sqlQuery, [id]);
     return result.rows[0];
 }
